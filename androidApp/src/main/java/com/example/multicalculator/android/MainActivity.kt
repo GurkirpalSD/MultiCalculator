d353bb9812ff6cebd9231823ba7703d451510c9e
Gurkirpal Singh <A00287624@mycambrian.ca>
2023-10-12 14:50:05 -0600

    calculator

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.multicalculator.android
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,11 +37,95 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     GreetingView(Greeting().greet())
+                    CalcView()
                 }
             }
         }
     }
 }
+@Composable
+fun CalcView() {
+    val displayText = remember { mutableStateOf("0") }
+
+    Column(modifier = Modifier.background(Color.Cyan)) {
+        Row {
+            CalcDisplay(display= displayText)
+        }
+        Row {
+            Column {
+                for (i in 7 downTo 1 step 3){
+                    CalcRow(displayText ,i,3)}
+
+
+                Row {
+                    CalcNumericButton(number = 0, display =displayText )
+                    CalcEqualsButton(display = displayText)
+                }
+            }
+            Column {
+                CalcOperationButton(operation = "+", display = displayText)
+                CalcOperationButton(operation = "-", display = displayText)
+                CalcOperationButton(operation = "*", display = displayText)
+                CalcOperationButton(operation = "/", display = displayText)
+            }
+
+        }
+    }
+}
+
+@Composable
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+    val endNum = startNum + numButtons
+    Row(modifier = Modifier.padding(0.dp)) {
+        for (i in startNum until endNum)
+            CalcNumericButton(number = i, display = display)
+    }
+}
+
+@Composable
+fun CalcDisplay(display: MutableState<String>) {
+    Text(
+        text = display.value,
+        modifier = Modifier
+            .height(50.dp)
+            .padding(5.dp)
+            .fillMaxWidth(1f)
+    )
+
+}
+
+@Composable
+fun CalcNumericButton(number: Int, display: MutableState<String>) {
+    Button(
+        onClick = {
+            display.value += number.toString()
+
+        }, modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = number.toString())
+    }
+
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(onClick = { display.value = "0" }, modifier = Modifier.padding(4.dp)) {
+        Text(text = "=")
+    }
+}
+
+@Composable
+fun CalcOperationButton(display: MutableState<String>, operation: String) {
+    Button(
+        onClick = {
+            display.value = ""
+        },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = operation)
+    }
+}
+
 
 @Composable
 fun GreetingView(text: String) {
